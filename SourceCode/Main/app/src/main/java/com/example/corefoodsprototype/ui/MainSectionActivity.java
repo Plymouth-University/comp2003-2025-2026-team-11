047991d4f2d8b35639f946a139907f75b8923e2c
Anubhav Dangal <dangalanubhav@gmail.com>
2026-01-01 14:43:37 +0000

    Added Prototype Hub launcher with navigation to Main, Chatbot and Settings sections.

---
+++ SourceCode/Main/app/src/main/java/com/example/corefoodsprototype/ui/MainSectionActivity.java
@@ -0,0 +1,26 @@
+package com.example.corefoodsprototype.ui;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.corefoodsprototype.R;
+
+public class MainSectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_section);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
