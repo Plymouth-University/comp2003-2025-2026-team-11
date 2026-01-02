a139857a0c7d04c08dc27185e56de5c76adbfb5c
Mehtab Ali <146185448+MRA1234567@users.noreply.github.com>
2026-01-02 15:24:43 +0000

    Implement CRUD operations in UserTable class
    Added methods insert, get, update and delete operations in UserTable.

--- SourceCode/MA/UserTable.java
+++ SourceCode/MA/UserTable.java
@@ -1,5 +1,9 @@
 package com.example.corefoodsdatabase;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 public class UserTable {
 
     public static final String TABLE_NAME = "User";
@@ -22,4 +26,36 @@ public class UserTable {
                     COL_DOB + " TEXT," +
                     COL_GENDER + " TEXT" +
                     ");";
+
+    // CREATE
+    public static long insert(SQLiteDatabase db, String email, String password, String name,
+                              double height, double weight, String dob, String gender) {
+        ContentValues values = new ContentValues();
+        values.put(COL_EMAIL, email);
+        values.put(COL_PASSWORD, password);
+        values.put(COL_NAME, name);
+        values.put(COL_HEIGHT, height);
+        values.put(COL_WEIGHT, weight);
+        values.put(COL_DOB, dob);
+        values.put(COL_GENDER, gender);
+        return db.insert(TABLE_NAME, null, values);
+    }
+
+    // READ
+    public static Cursor getByEmail(SQLiteDatabase db, String email) {
+        return db.query(TABLE_NAME, null, COL_EMAIL + "=?",
+                new String[]{email}, null, null, null);
+    }
+
+    // UPDATE
+    public static int update(SQLiteDatabase db, String email, ContentValues values) {
+        return db.update(TABLE_NAME, values, COL_EMAIL + "=?",
+                new String[]{email});
+    }
+
+    // DELETE
+    public static int delete(SQLiteDatabase db, String email) {
+        return db.delete(TABLE_NAME, COL_EMAIL + "=?",
+                new String[]{email});
+    }
 }
