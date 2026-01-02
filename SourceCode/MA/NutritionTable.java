b1a09f1461408a0a18c064e10fb24c72a7cb42c2
Mehtab Ali <146185448+MRA1234567@users.noreply.github.com>
2026-01-02 15:23:15 +0000

    Implement insert and query methods in NutritionTable
    Added methods for inserting and retrieving data.

--- SourceCode/MA/NutritionTable.java
+++ SourceCode/MA/NutritionTable.java
@@ -1,5 +1,9 @@
 package com.example.corefoodsdatabase;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 public class NutritionTable {
 
     public static final String TABLE_NAME = "Nutrition";
@@ -19,4 +23,21 @@ public class NutritionTable {
                     COL_FAT + " REAL," +
                     "FOREIGN KEY (" + COL_FOOD_ID + ") REFERENCES Food(FoodID)" +
                     ");";
+
+    public static long insert(SQLiteDatabase db, int foodId, int calories,
+                              double protein, double carbs, double fat) {
+        ContentValues values = new ContentValues();
+        values.put(COL_FOOD_ID, foodId);
+        values.put(COL_CALORIES, calories);
+        values.put(COL_PROTEIN, protein);
+        values.put(COL_CARBS, carbs);
+        values.put(COL_FAT, fat);
+        return db.insert(TABLE_NAME, null, values);
+    }
+
+    public static Cursor get(SQLiteDatabase db, int foodId) {
+        return db.query(TABLE_NAME, null, COL_FOOD_ID + "=?",
+                new String[]{String.valueOf(foodId)}, null, null, null);
+    }
 }
+
