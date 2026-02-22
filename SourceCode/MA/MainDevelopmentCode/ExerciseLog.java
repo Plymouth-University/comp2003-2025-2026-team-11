d6ea18e4816fc50ab8959bc3167ce8a1b208bb46
Mehtab Ali <146185448+MRA1234567@users.noreply.github.com>
2026-02-22 18:53:00 +0000

    Add setter methods to ExerciseLog class
    Added setter methods for ExerciseLog variables.

--- SourceCode/MA/MainDevelopmentCode/ExerciseLog.java
+++ SourceCode/MA/MainDevelopmentCode/ExerciseLog.java
@@ -25,5 +25,13 @@ public class ExerciseLog {
     public String getEL_CALORIES_BURNED() { return EL_CALORIES_BURNED; }
     public String getEL_TIME() { return EL_TIME; }
     public String getEL_NOTES() { return EL_NOTES; }
+
+    public void setEL_EXERCISE_TYPE(String EL_EXERCISE_TYPE) { this.EL_EXERCISE_TYPE = EL_EXERCISE_TYPE; }
+    public void setEl_INTENSITY(String El_INTENSITY) { this.El_INTENSITY = El_INTENSITY; }
+    public void setEL_DURATION_MINS(String EL_DURATION_MINS) { this.EL_DURATION_MINS = EL_DURATION_MINS; }
+    public void setEL_CALORIES_BURNED(String EL_CALORIES_BURNED) { this.EL_CALORIES_BURNED = EL_CALORIES_BURNED; }
+    public void setEL_TIME(String EL_TIME) { this.EL_TIME = EL_TIME; }
+    public void setEL_NOTES(String EL_NOTES) { this.EL_NOTES = EL_NOTES; }
 }
+
 
